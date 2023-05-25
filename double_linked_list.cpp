c09568d8909b1a58c96ab25694c29397b569b051
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-25 11:47:24 +0700

    15

--- double_linked_list.cpp
+++ double_linked_list.cpp
@@ -126,5 +126,21 @@ void DoubleLinkedList::descending() {
 	}
 }
 
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nList is empty" << endl;
+	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted : ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Rcord with roll number " << rollNo << " deleted" << endl;
+}
+
+
+
 
 
