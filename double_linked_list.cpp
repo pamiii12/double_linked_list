e3dfe1ccf9647538934e38cf37c7f7f9098bc40a
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-25 10:27:19 +0700

    4

--- double_linked_list.cpp
+++ double_linked_list.cpp
@@ -15,6 +15,12 @@ private:
 	node* START;
 public:
 	DoubleLInkedList();
-
-
+	void addNode();
+	bool search(int rollno, node* previous, node** current);
+	bool deleteNode(int rollNode);
+	bool listEmpty();
+	void ascending();
+	void discending();
+	void hapus();
+	void searchData();
 };
